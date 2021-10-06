3718a75d01e180d62dab4bb502411ebb882d8148
theimm0rtal <pritthakkar111101@gmail.com>
2021-10-06 21:36:48 +0530

    init

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+</div> <!-- container -->
+	
+	<footer>
+		<div class="col-md-6 col-xs-6"><p>Copyright &copy; 2018 <a href="https://www.immortal.org.in">Immortal Corporation</a>, All rights reserved.</p></div>
+  		<div class="col-md-6 col-xs-6 text-right"><p>Made with ❤️ by <a href="https://www.immortal.org.in">Immortal Corporation</a></p></div>
+	</footer>
+	<!-- file input -->
+	<script src="assests/plugins/fileinput/js/plugins/canvas-to-blob.min.js'); ?>" type="text/javascript"></script>	
+	<script src="assests/plugins/fileinput/js/plugins/sortable.min.js" type="text/javascript"></script>	
+	<script src="assests/plugins/fileinput/js/plugins/purify.min.js" type="text/javascript"></script>
+	<script src="assests/plugins/fileinput/js/fileinput.min.js"></script>	
+
+
+	<!-- DataTables -->
+	<script src="assests/plugins/datatables/jquery.dataTables.min.js"></script>
+
+</body>
+</html>
